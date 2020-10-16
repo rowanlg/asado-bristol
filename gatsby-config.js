c9ffa1cbd2178f6489f5940dd198bf14ab5dd0f6
Rowan Gordon <rowan@rowangordon.co.uk>
2020-10-16 09:32:35 +0100

    added site metadata, and fixed some sizing issues with text shadow

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Asado Barcelona`,
+    description: `Asado has come to Barcelona!`,
+    author: `Asado Restaurant LTD`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
